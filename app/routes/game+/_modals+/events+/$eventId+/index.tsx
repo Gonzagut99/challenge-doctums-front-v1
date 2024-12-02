c172b25a6e0a24f2f384cb03a79c3b58bad342cd
Esclender <elesclenderlugo@gmail.com>
2024-12-02 17:32:36 -0500

    refactor: remove unused loadAllModifiersData import and update data loading method

--- app/routes/game+/_modals+/events+/$eventId+/index.tsx
+++ app/routes/game+/_modals+/events+/$eventId+/index.tsx
@@ -8,7 +8,7 @@ import { EfficiencyPointsTile } from "~/components/custom/EfficienyTile";
 import Modal from "~/components/custom/Modal";
 import { EfficiencyTableTileData } from "~/types/efficiencies";
 import { ModifierFeature } from "~/types/modifiers";
-import { initializedDataLoader, loadAllModifiersData } from "~/utils/dataLoader";
+import { initializedDataLoader } from "~/utils/dataLoader";
 
 
 export type consequenceResult = {
@@ -40,7 +40,7 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
     // const products = await loadProducts("app/data/products.csv");
     // const projects = await loadProducts("app/data/projects.csv");
     // const resources = await loadProducts("app/data/resources.csv");
-    const { products, projects, resources } = allLoadedData;
+    const { products, projects, resources } = initializedDataLoader.getAllData();
     const requiredEficciencies = selectedEvent.required_efficiencies.map((efficiency) => efficiencies[efficiency]);
     const modifiableProducts = selectedEvent.modifiable_products.map((product) => products[product]);
     const modifiableProjects = selectedEvent.modifiable_projects.map((project) => projects[project]);
